c013a5a0c17d42b35f646fd3f8a920e683ef40d6
EzeG92 <102333584+EzeG92@users.noreply.github.com>
2024-05-22 20:13:19 -0300

    Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Drawing;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 15);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Red", 10, 20);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Yellow", 20);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
+        
     }
 }
